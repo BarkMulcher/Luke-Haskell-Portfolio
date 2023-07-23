6f39fb36de1bca0e71a57db828467212e5f82af7
burpmonster <lucas.c.haskell@gmail.com>
2023-07-23 19:59:15 -0400

    add index route to app.js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Redirect, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <BrowserRouter basename='/'>
       <div>
         <Navbar />
       </div>
       <div className='container'>
         <Routes>
-          <Route path="/" element={<About />} />
+          <Route index element={<About />} />
           <Route path='/portfolio' element={<Portfolio />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Redirect from='*' to='/' />
         </Routes>
       </div>
       <div className='footer'>
         <Footer />
       </div>
+    </BrowserRouter>
     </>
   );
 }
