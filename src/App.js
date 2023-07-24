a0e532564e83cb86becc13d201bb3368ed317fa3
burpmonster <lucas.c.haskell@gmail.com>
2023-07-23 21:37:02 -0400

    added home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
+import Home from './components/Home';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -15,11 +16,12 @@ function App() {
       </div>
       <div className='container'>
         <Routes>
-          <Route path='/' element={<About />} />
+          <Route path='/' element={<Home />} />
           <Route path='/portfolio' element={<Portfolio />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+
       </div>
       <div className='footer'>
         <Footer />
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div className='container'>
+        <div className='row'>
+            <div className='col-md-12'>
+            <div className='jumbotron'>
+                <h1 className='display-4'>Luke Haskell</h1>
+                <p className='lead'>Full Stack Web Developer</p>
+                <hr className='my-4' />
+                <h1>HOME PAGE</h1>
+            </div>
+            </div>
+        </div>
+        </div>
+        )
+}
