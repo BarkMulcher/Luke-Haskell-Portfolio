f2f6fc2bfb88ad1db10d41a0027069509fb0a0d8
burpmonster <lucas.c.haskell@gmail.com>
2023-08-01 16:21:13 -0400

    add resume download link and rename resume w/ underscores

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,13 @@ const Resume = () => {
 
     return (
       <section className="resume-section">
-        <a className="download-button" href="https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/resume/----------------.pdf" target='_blank' rel="noreferrer" download="Nick Zabkar Resume">Click Here To Download</a>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+        <a className="download-button" href="https://raw.githubusercontent.com/BarkMulcher/luke-haskell-portfolio/src/data/Lucas_Haskell_Resume_7.31.23" target='_blank' rel="noreferrer" download="Nick Zabkar Resume">Click Here To Download</a>
         <article className='resume-pdf'>
           <iframe title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vSqpYEMht1hhOle8rMKiuE_JT8c97KYkMscPMw85DvTlmFtwqPnNKn7f-0mNoQRVVvatsPjQgq_Meyc/pub?embedded=true"></iframe>
         </article>
