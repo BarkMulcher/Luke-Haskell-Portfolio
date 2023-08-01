80b7b455b985b7580e35d105d5f087396fe2c276
burpmonster <lucas.c.haskell@gmail.com>
2023-08-01 17:03:10 -0400

    fix download link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,7 +10,7 @@ const Resume = () => {
       <br/>
       <br/>
       <br/>
-        <a className="download-button" href="https://raw.githubusercontent.com/BarkMulcher/luke-haskell-portfolio/src/data/Lucas_Haskell_Resume_7.31.23" target='_blank' rel="noreferrer" download="Nick Zabkar Resume">Click Here To Download</a>
+        <a className="download-button" href="https://raw.githubusercontent.com/BarkMulcher/luke-haskell-portfolio/main/src/data/Lucas_Haskell_Resume_7.31.23.pdf" target='_blank' rel="noreferrer" download="Lucas Haskell Resume">Click Here To Download</a>
         <article className='resume-pdf'>
           <iframe title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vSqpYEMht1hhOle8rMKiuE_JT8c97KYkMscPMw85DvTlmFtwqPnNKn7f-0mNoQRVVvatsPjQgq_Meyc/pub?embedded=true"></iframe>
         </article>
